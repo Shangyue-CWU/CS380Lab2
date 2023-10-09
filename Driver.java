d0e22e05a4b189fcb2682a8a40152882cb519ee0
j-melchor <146897927+j-melchor@users.noreply.github.com>
2023-10-09 16:11:55 -0700

    Delete Driver.java
    Accidental Submission

--- Driver.java
+++
@@ -1,9 +0,0 @@
-public class Driver {
-    public static void main(String[] args) {
-        System.out.println();
-        Elk elk = new Elk("Elk");
-        elk.sound();
-        elk.play();
-        elk.eat();
-    }
-}
