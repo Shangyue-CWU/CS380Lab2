7b04d3a0ff124ca43f078b59bfe68ae8a936dd39
Garrett Holden <Holden_170_@hotmail.com>
2023-10-11 12:27:32 -0700

    Completed Duck file

--- Duck.java
+++ Duck.java
@@ -1,2 +1,33 @@
 public class Duck extends Animal{
+
+    //Field to hold name of duck
+    protected String name;
+    
+    //constructor takes name of duck
+    public Duck(String name){
+       super(name);
+       this.name = name; 
+    }
+    
+    public void sound(){
+       System.out.println(this.name + " suddenly makes the sound QUAAAACKKKK!!");
+    }
+   
+   
+   /**
+    * Abstract method that should represent a specific  activity
+    * that an animal does for fun.
+    */
+    public void play(){
+       System.out.println(this.name + " flies from sky and swims in nearby pond");
+    }
+   
+   
+   /**
+    * A  method that prints out that the animal is eating food.
+    */
+    public void eat(){
+      System.out.println(this.name + " eats bugs in a pond.");
+   } 
+    
 }
