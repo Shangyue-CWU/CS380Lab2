9ff7af9252d67add059fb15fa1915664c137edc8
Garrett Holden <Holden_170_@hotmail.com>
2023-10-11 12:27:50 -0700

    Completed Test File

--- Test.java
+++ Test.java
@@ -1,2 +1,10 @@
 public class Test {
+
+   public static void main(String args[]){
+      Duck duck1 = new Duck("Flappy");
+      duck1.play();
+      duck1.sound();
+      duck1.eat(); 
+      
+   }
 }
